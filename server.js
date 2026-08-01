c551105a842307182b089f0b7d3df2f2bd22f712
bhavishpixo77 <Bhavish@pixoramagroup.com>
2026-08-02 00:23:07 +0530

    lint issue fixed

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 async function ConnectDb() {
-  await mongoose.connect("mongodb://mongodb:27017/connectionmadesuccess");
+  const uri = process.env.MONGO_URI || "mongodb://localhost:27017/connectionmadesuccess";
+  await mongoose.connect(uri);
   console.log("connection done");
 }
 
